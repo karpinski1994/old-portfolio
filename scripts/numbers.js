dc562b28062262fcd77072bb330840349339da5a
patryk <patryk.karpinski@outlook.com>
2018-01-18 17:27:15 +0100

    sass index changes

--- scripts/numbers.js
+++ scripts/numbers.js
@@ -15,37 +15,47 @@ function init(){
     
         const projects = document.querySelector('.projects');
         const features = document.getElementById('features');
-        const top = projects.offsetTop - 500;
+        const top = projects.offsetTop - 650;
         const number = document.getElementById('number');
         let currentOffset = window.pageYOffset;
         let moveValue = window.pageYOffset/1.20;
         let currentNumber = 1;
         let currentNumberPosition = 0;
+        
+        
+        let begin = 195;
+        let offset = 350;
+        
+        
         if(currentOffset >=top){
             number.textContent = currentNumber;
             number.style.display = "block";
             moveNumber();
         }
         
+        
+        
        function moveNumber(){
            number.style.top = 'calc(410px + '+ moveValue +'px)';
-           if(currentOffset < 410){
+           
+           
+           if(currentOffset < begin){
                number.style.display = "none";
            }
-           else if(currentOffset > 440 &currentOffset <770){
+           else if(currentOffset >= begin & currentOffset < begin + offset){
                currentNumber = 1;
                number.textContent = currentNumber;
            }
-           else if(currentOffset > 770 & currentOffset < 1115){
+           else if(currentOffset > begin + offset & currentOffset < begin + (2*offset)){
                currentNumber = 2;
                number.textContent = currentNumber;
-           }else if(currentOffset > 1115 & currentOffset <1405) {
+           }else if(currentOffset > begin + (2*offset) & currentOffset < begin + (3*offset)) {
                currentNumber = 3;
                number.textContent = currentNumber;
-           }else if(currentOffset > 1405 & currentOffset <1800) {
+           }else if(currentOffset > begin + (3*offset) & currentOffset <begin + (4*offset)) {
                currentNumber = 4;
                number.textContent = currentNumber;
-           }else if(currentOffset > 1790 & currentOffset <2035) {
+           }else if(currentOffset > begin + (4*offset) & currentOffset <begin + (5*offset)) {
                currentNumber = 5;
                number.textContent = currentNumber;
            }else{
